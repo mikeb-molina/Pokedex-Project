af91e523ee7db838956d9a39411981c37ff999f7
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2023-09-15 10:02:34 -0400

    Created and added 3 new Pokemon to pokemonlist

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let pokemonlist=[];
+pokemonlist[
+    {
+        name: 'Bulbasaur',
+        type: ['Grass', 'Poison'],
+        height: '0.7',
+        hp: '45'
+
+    },
+    {
+        name: 'Sandshrew',
+        type: 'Ground',
+        height: '0.6',
+        hp: '50'
+    },
+    {
+        name: 'Cubone',
+        type: 'Ground',
+        height: '0.4',
+        hp: '50'
+    }
+]
