7d0ee98277531ca1fa2cdb579afc215c8dfc3546
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2023-09-17 11:59:28 -0400

    Adding pokemon list to MOD

--- js/scripts.js
+++ js/scripts.js
@@ -19,4 +19,24 @@ pokemonList[
         height: '0.4',
         hp: '50'
     }
-]
+];
+
+//define speacial height for pokemon
+    specialHeight = 0.7;
+
+// Iterate each pokemon in array
+for (let i = 0; i < pokemonList.length; i++){
+    //grab name and height of each pokemon in array
+    console.log (pokemonName = pokemonList[i].name);
+    console.log (pokemonHeight = pokemonList[i].height);
+
+    //create variable to store label
+    let label= ' ';
+
+    //check for special height among pokemon
+    if (pokemonHeight > specialHeight) {
+        label = "- Wow that's a big pokemon!"
+    }
+
+    document.write(`$pokemonName} (height: ${pokemonHeight}${label})<br>`);
+}
