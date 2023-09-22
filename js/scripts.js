0ac84074125d681d1cb18245a5d69afa1f839f44
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2023-09-22 09:40:01 -0400

    restructured code for 'Wow thats big' special pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -26,16 +26,12 @@ for (let i = 0; i < pokemonList.length; i++){
     //grab name and height of each pokemon in array
     console.log (pokemonName = pokemonList[i].name);
     console.log (pokemonHeight = pokemonList[i].height);
-
-       //write pokemon name and height on DOM
-    document.write (`${pokemonName} (height: ${pokemonHeight}) <br>`);
     
     //define threshhold height for pokemon
     let thresholdHeight = '0.7';
 
     //check for threshhold height among pokemon
     if (pokemonList[i].height >= thresholdHeight){
-       document.write ( ' -Wow that\s big!');
-    }
-
+        document.write (`${pokemonName} (height: ${pokemonHeight}) -Wow that\s big! <br>`);
+    } else{ document.write (`${pokemonName} (height: ${pokemonHeight}) <br>`);}
 }
