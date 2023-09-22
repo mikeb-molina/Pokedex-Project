7e9f95dd6d46263fb85378fed122b11077f582d6
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2023-09-22 17:22:25 -0400

    Created IIFE for pokemon List and introduced forEach loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,35 @@
-let pokemonList=[
-    {
-        name: 'Bulbasaur',
-        type: ['Grass', 'Poison'],
-        height: '0.7',
-        hp: '45'
+// created IIFE for pokemonList
+let poekmonRepository = (function () {
+    let pokemonList=[];
+   
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        };
+        function getAll() {
+            return pokemonList;
+        };
+        return{
+            add: add,
+            getAll: getAll
+        };
+})();
 
-    },
-    {
-        name: 'Sandshrew',
-        type: 'Ground',
-        height: '0.6',
-        hp: '50'
-    },
-    {
-        name: 'Cubone',
-        type: 'Ground',
-        height: '0.4',
-        hp: '50'
-    }
-];
+// Define information for Pokemon List
+poekmonRepository.add({name: 'Bulbasaur', type:['Grass', ' Poison'], height: '0.7 Meters'});
+poekmonRepository.add({name: 'Sandshrew', type: 'Ground', height: '0.6 Meters'});
+poekmonRepository.add({name: 'Cubone', type: 'Ground', height: '0.4 Meters'});
 
+// Write Pokemon information on DOM
+poekmonRepository.getAll().forEach(function(pokemonList){;
+    document.write(`${pokemonList.name}: ${pokemonList.type}, ${pokemonList.height} <br>`);
 
-// Iterate each pokemon in array
-for (let i = 0; i < pokemonList.length; i++){
-    //grab name and height of each pokemon in array
-    console.log (pokemonName = pokemonList[i].name);
-    console.log (pokemonHeight = pokemonList[i].height);
-    
-    //define threshhold height for pokemon
+    // Define threshhold height for pokemon
     let thresholdHeight = '0.7';
 
-    //check for threshhold height among pokemon
-    if (pokemonList[i].height >= thresholdHeight){
-        document.write (`${pokemonName} (height: ${pokemonHeight}) -Wow that\s big! <br>`);
-    } else{ document.write (`${pokemonName} (height: ${pokemonHeight}) <br>`);}
-}
+    // Check for threshhold height among pokemon
+    if (pokemonList.height >= thresholdHeight){
+        document.write (`- Wow that\s big! <br>`);
+    };
+});
+    
+    
